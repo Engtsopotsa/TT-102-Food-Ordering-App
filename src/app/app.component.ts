5fce6fea7213b5bc85662ffa4e9af028294c08ee
Tapiwa Tsopotsa <tapiwatsopotsa1@gmail.com>
2024-05-02 12:06:16 +0200

    login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './core/header/header.component';  // Ensure correct path
+import { HeaderComponent } from './core/header/header.component'; // Ensure correct path
+import { SignInComponent } from './auth/sign-in/sign-in.component'; // Ensure correct path
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],// Import HeaderComponent if used in the template
+  imports: [RouterOutlet, HeaderComponent, SignInComponent], // Import HeaderComponent if used in the template
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   // Title or any other global properties can be defined here
   title = 'TT-102-Food-Ordering-App';
 }
-
-
